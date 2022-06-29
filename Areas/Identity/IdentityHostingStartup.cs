3cc9295766075ba501787c676d7226897becf8a8
Mourya Pradeep Ramashare <mourya.18bcs1117@abes.ac.in>
2022-06-29 20:13:51 +0530

    Changes Login and Logout

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Hospital.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Hospital.Areas.Identity.IdentityHostingStartup))]
+namespace Hospital.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<HospitalDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("HospitalDBContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<HospitalDBContext>();
+            });
+        }
+    }
+}
